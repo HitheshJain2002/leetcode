6ff502c64b35b7e89aa5a2e3d138a413bf44e7c7
HitheshJain2002 <138424080+HitheshJain2002@users.noreply.github.com>
2024-02-07 20:31:23 +0530

    Time: 2 ms (82.80%) | Memory: 46.4 MB (15.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,21 +1,14 @@
 class Solution {
-    public int[] moveZeroes(int[] nums) {
+    public void moveZeroes(int[] nums) {
         int n = nums.length;
-        int count = 0; // Keep track of the position to place non-zero elements
-        
-        // Iterate through the array
+        int count = 0;
         for (int i = 0; i < n; i++) {
-            // If the current element is non-zero, move it to the next available position
             if (nums[i] != 0) {
-                nums[count] = nums[i];
-                // If we moved the non-zero element to a new position, increment count
-                if (i != count) {
-                    nums[i] = 0; // Assign the current position with zero
-                }
-                count++;
+                nums[count++] = nums[i];
             }
         }
-        
-        return nums;
+        while (count < n) {
+            nums[count++] = 0;
+        }
     }
 }
